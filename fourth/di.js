10867dc506e0a6db1b258ab4e030865797a4cd74
zhufengzhufeng <894918097@qq.com>
2016-12-15 16:28:58 +0800

    $on $emit $broadcast

---
+++ fourth/di.js
@@ -0,0 +1,13 @@
+var obj = {
+    $timeout:function (cb) {
+        console.log('$timeout');cb()
+    },
+    $interval:function (cb) {
+        console.log('interval');cb()
+    }
+};
+//将函数转换成字符串进行参数匹配
+function cc(  $timeout   ,  $interval  ) {}
+//第一步先取出（）中的内容，替换掉空格，以逗号分割
+cc.toString().match(/\((.*)\)/)[1].replace(/\s+/g,'').split(',');
+console.log(cc.toString().match(/\((.*)\)/)[1].replace(/\s+/g,'').split(','));
