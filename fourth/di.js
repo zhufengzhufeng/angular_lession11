6b19d10bcffaa6dab0a273fb822f28c075c93c30
zhufengzhufeng <894918097@qq.com>
2016-12-16 18:05:56 +0800

    11

--- fourth/di.js
+++ fourth/di.js
@@ -1,4 +1,4 @@
-var obj = {
+﻿var obj = {
     $timeout:function (cb) {
         console.log('$timeout');cb()
     },
@@ -11,3 +11,8 @@ function cc(  $timeout   ,  $interval  ) {}
 //第一步先取出（）中的内容，替换掉空格，以逗号分割
 cc.toString().match(/\((.*)\)/)[1].replace(/\s+/g,'').split(',');
 console.log(cc.toString().match(/\((.*)\)/)[1].replace(/\s+/g,'').split(','));
+
+
+
+
+
